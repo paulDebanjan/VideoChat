0d5a21ba75cce438c6dfe480ffe182457f2f67e3
radharanipaul <debanjandevelopment@gmail.com>
2022-10-14 18:49:50 +0600

    add stan server

--- public/chat.js
+++ public/chat.js
@@ -21,11 +21,9 @@ let rtcPeerConnection;
 let userStream;
 
 let iceServers = {
-    
     iceServers:[
-        {urls: "stan:numb.viagenie.ca:3478"},
         {urls: "stun:stun.services.mozilla.com"},
-        // {urls: "stun:stun.l.google.com:19302"},  
+        {urls: "stun:stun.l.google.com:19302"},
     ],
 } 
 
